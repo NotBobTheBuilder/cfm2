3631c1530374613ed39764480211ad7b24e28401
Jack Wearden <jack@jackwearden.co.uk>
2013-07-16 15:42:46 +0100

    issue #10 - adds Collection for Control page

---
+++ classes/Collection/Control.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * CampFire Manager is a scheduling tool predominently used at BarCamps to 
+ * schedule talks based, mainly, on the number of people attending each talk
+ * receives.
+ *
+ * PHP version 5
+ *
+ * @category Default
+ * @package  CampFireManager2
+ * @author   Jon Spriggs <jon@sprig.gs>
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
+ * @link     https://github.com/CampFireManager/cfm2 Version Control Service
+ */
+/**
+ * This serves as a control panel for admins to manage the event
+ *
+ * @category Collection_Control
+ * @package  CampFireManager2
+ * @author   Jack Wearden <jack@jackwearden.co.uk>
+ * @license  http://www.gnu.org/licenses/agpl.html AGPLv3
+ * @link     https://github.com/CampFireManager/cfm2 Version Control Service
+ */
+
+class Collection_Control extends Abstract_GenericCollection
+{
+    /**
+     * Collect the data for this collection
+     *
+     * @param integer|null $screen The screen ID to return, or null to create a
+     * new screen and reload to that page.
+     * 
+     * @return Collection_Control
+     */
+    protected function __construct()
+    {
+        $this->arrData['arrRooms'] = array();
+        $this->arrData['arrSlots'] = array();
+
+        foreach (Object_Room::brokerAll() as $k => $v) {
+            array_push($this->arrData['arrRooms'], $v->getData());
+        }
+        foreach (Object_Slot::brokerAll() as $k => $v) {
+            array_push($this->arrData['arrSlots'], $v->getData());
+        }
+
+        return $this;
+    }
+}
